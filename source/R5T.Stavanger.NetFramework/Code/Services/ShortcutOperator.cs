4b7459e267802534b1478700e8ffeab141c3a920
MinexAutomation <minex.automation@gmail.com>
2020-02-11 00:56:16 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Stavanger.NetFramework/Code/Services/ShortcutOperator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R5T.Stavanger.NetFramework.Code.Services
+{
+    class ShortcutOperator
+    {
+    }
+}
